865d509531c8979aee9cd496b2393ce209fd6177
booldook <booldook@gmail.com>
2018-11-12 13:51:13 +0900

    ...

---
+++ include/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>테스트</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="wrap">
---
+++ include/top.php
@@ -0,0 +1,13 @@
+<div class="top_menu clear">
+	<ul>
+		<li>Sitemap</li>
+		<li>Join</li>
+		<li>Login</li>
+	</ul>
+</div>
+<div class="top clear">
+	<div class="logo">LOGO</div>
+	<div class="bars">
+		<span class="fa fa-bars"></span>
+	</div>
+</div>
